0ad4d67ae79f362be2508157602a4bc494b18cae
AdhifCode <nadhifalazharuddiya@gmail.com>
2022-12-20 16:15:42 +0700

    string

--- MentorRYAN/Pertemuan2/stringToArray.php
+++ MentorRYAN/Pertemuan2/stringToArray.php
@@ -1,4 +1,6 @@
 <?php
-$kalimat = "Aku suka partai banteng tapi boong";
-print_r (explode(" ",$kalimat));
-?> 
+$str = "Aku suka partai banteng, tapi boong";
+print_r(str_word_count($str, 1, 'aaaa'));
+
+echo str_word_count($str);
+?>
