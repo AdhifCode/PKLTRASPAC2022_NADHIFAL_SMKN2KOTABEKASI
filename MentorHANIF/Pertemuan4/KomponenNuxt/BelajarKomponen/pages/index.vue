6511dc9962acb67e419a521f8e15b361b3b355a7
AdhifCode <nadhifalazharuddiya@gmail.com>
2023-01-02 16:50:13 +0700

    komponen

---
+++ MentorHANIF/Pertemuan4/KomponenNuxt/BelajarKomponen/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+
+export default {
+  name: 'IndexPage',
+  components: { Header },
+}
+</script>
