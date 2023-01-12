719d92f67082bb3f4a4193fb2ff8be53dbf79700
AdhifCode <nadhifalazharuddiya@gmail.com>
2023-01-12 16:58:19 +0700

    kalkulator

---
+++ MentorFAJAR/Pertemuan5/kalkulator_nuxt/layouts/layoutme.vue
@@ -0,0 +1,34 @@
+<template>
+   <div>
+    <div>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-nav>
+      <b-nav-item href="#">Home</b-nav-item>
+
+      <!-- Navbar dropdowns -->
+      <b-nav-item-dropdown text="Lang" right>
+        <b-dropdown-item href="#">EN</b-dropdown-item>
+        <b-dropdown-item href="#">ES</b-dropdown-item>
+        <b-dropdown-item href="#">RU</b-dropdown-item>
+        <b-dropdown-item href="#">FA</b-dropdown-item>
+      </b-nav-item-dropdown>
+
+      <b-nav-item-dropdown text="User" right>
+        <b-dropdown-item href="#">Account</b-dropdown-item>
+        <b-dropdown-item href="#">Settings</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-navbar-nav>
+  </b-navbar>
+</div>
+
+<div>
+    <Nuxt />
+</div>
+
+<footer>
+  <p>Author: Hege Refsnes<br>
+  <a href="mailto:hege@example.com">hege@example.com</a></p>
+</footer>
+
+</div>
+</template>
---
+++ MentorFAJAR/Pertemuan5/kalkulator_nuxt/pages/index.vue
@@ -0,0 +1,81 @@
+<template>
+<div>
+  <div class="card">
+    <div class="container">
+      <select id="metode" v-model="metode">
+        <optgroup label="Pilih Aritmatika">
+      <option value="jumlahan">Penjumlahan</option>
+      <option value="kurangan">Pengurangan</option>
+      <option value="kalian">Perkalian</option>
+      <option value="bagian">Pembagian</option>
+      <option value="modulo">Modulo</option>
+          </optgroup>
+      </select>
+    </div>
+    <div class="row">
+      <input type="number" v-model="angka1" placeholder="Masukan Bilangan">
+    </div>
+    <div class="row">
+      <input type="number" v-model="angka2" placeholder="Masukan Bilangan">
+    </div>
+    <div class="row submitan">
+      <div class="submit">
+        <input @click.prevent="submit()" type="submit" value="Submit"/>
+    </div>
+    </div>
+    <div v-if="this.set === true" class="hasil">
+      <h3>Hasil : {{ hasil }}</h3>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  layout : 'layoutme',
+  name: 'IndexPage',
+  data() {
+    return {
+      set : false,
+
+      angka1 : null,
+      angka2 : null,
+      metode : null,
+      hasil : null
+    };
+  },
+  methods: {
+    submit() {
+    if(this.angka1 === null || this.angka2 === null || this.angka1 === '' || this.angka2 === ''){
+      alert("Input Belum Benar")
+    } else {
+      if(this.metode === null || this.metode === ''){
+        alert("Piliha Metode Dahulu");
+      }
+    else if(this.metode === 'jumlahan'){
+      this.hasil = parseInt(this.angka1) + parseInt(this.angka2);
+      this.set = true;
+    }  
+    else if(this.metode === 'kurangan'){
+      this.hasil = parseInt(this.angka1) - parseInt(this.angka2);
+      this.set = true;
+    }  
+    else if(this.metode === 'kalian'){
+      this.hasil = parseInt(this.angka1) * parseInt(this.angka2);
+      this.set = true;
+    }  
+    else if(this.metode === 'bagian'){
+      this.hasil = parseInt(this.angka1) / parseInt(this.angka2);
+      this.set = true;
+    }  
+    else if(this.metode === 'modulo'){
+      this.hasil = parseInt(this.angka1) % parseInt(this.angka2);
+      this.set = true;
+    }  
+    }
+    
+},
+  },
+}
+  
+</script>
